c712858d16a2d78cb190b32e9dd0149f00c266a6
cyberjacob <cyberjacob@gmail.com>
2020-07-04 18:30:03 +0100

    Initial implementation of account retrieval API

--- savings/views.py
+++ savings/views.py
@@ -1,8 +1,28 @@
+import json
+
 from django.http import HttpResponse, JsonResponse
 from django.shortcuts import render
 from django.views.decorators.csrf import csrf_exempt
 
 from . import models
+
+ACCOUNT_DEF = [
+    {"text": "Starting Balance",    "type": "number"},
+    {"text": "Current Balance",     "type": "number"},
+    {"text": "Total Topup",         "type": "number"},
+    {"text": "Average APR",         "type": "number"},
+    {"text": "Returns",             "type": "number"},
+    {"text": "Balance OK",          "type": "number"},
+    {"text": "Name",                "type": "string"},
+    {"text": "Bank Name",           "type": "string"},
+    {"text": "Account Name",        "type": "string"},
+    {"text": "Account Number",      "type": "string"},
+    {"text": "Sort Code",           "type": "string"},
+    {"text": "Predicted Interest",  "type": "number"},
+    {"text": "Interest Min",        "type": "number"},
+    {"text": "Interest Max",        "type": "number"},
+    {"text": "Instant Withdrawal",  "type": "number"},
+]
 
 
 # Create your views here.
@@ -20,7 +40,62 @@ def search(request):
 
 @csrf_exempt
 def query(request):
-    return JsonResponse("", safe=False)
+    response = []
+    data = json.loads(request.body)
+    for target in data["targets"]:
+        if target["target"] == "accounts":
+            if target["data"]["pk"] is not None:
+                account = models.Account.objects.get(pk=target["data"]["pk"])
+                response += {
+                    "columns":ACCOUNT_DEF,
+                    "rows":[
+                        [
+                            account.starting_balance,
+                            account.current_balance,
+                            account.total_topup,
+                            account.average_APR,
+                            account.returns,
+                            account.balance_OK,
+                            account.__str__(),
+                            account.bank_name,
+                            account.account_name,
+                            account.account_number,
+                            account.sort_code,
+                            account.predicted_interest,
+                            account.interest_min,
+                            account.interest_max,
+                            account.instant_withdrawal
+                        ],
+                    ],
+                    "type": "table"
+                }
+            else:
+                accounts = models.Account.objects.all()
+                response += {
+                    "columns":ACCOUNT_DEF,
+                    "rows":[
+                        [[
+                            account.starting_balance,
+                            account.current_balance,
+                            account.total_topup,
+                            account.average_APR,
+                            account.returns,
+                            account.balance_OK,
+                            account.__str__(),
+                            account.bank_name,
+                            account.account_name,
+                            account.account_number,
+                            account.sort_code,
+                            account.predicted_interest,
+                            account.interest_min,
+                            account.interest_max,
+                            account.instant_withdrawal
+                        ] for account in accounts],
+                    ],
+                    "type": "table"
+                }
+
+    return JsonResponse(response, safe=False)
 
 
 @csrf_exempt
