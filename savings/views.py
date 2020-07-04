1c969686aef38d63d842e3f33114cb900c2d56ea
cyberjacob <cyberjacob@gmail.com>
2020-07-04 17:56:57 +0100

    Initial testing for Grafana API

--- savings/views.py
+++ savings/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from . import models
 
 # Create your views here.
+def test():
+    """Used by Grafana to test basic connectivity"""
+    return "OK"
+
+def search():
+    """Used by Grafana to find metrics"""
+    return '["accounts", "balances"]'
+
+def query():
+    return ""
+
+def annotations():
+    return "[]"
--- savings_tracker/urls.py
+++ savings_tracker/urls.py
@@ -15,7 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from savings import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.test),
+    path('search', views.search),
+    path('query', views.query),
+    path('annotations', views.annotations),
 ]
