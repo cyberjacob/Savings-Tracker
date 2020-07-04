a8d2ddb0b53ba31280f01e391c87499ff7ce7842
cyberjacob <cyberjacob@gmail.com>
2020-07-04 17:58:39 +0100

    Disable CSRF for API

--- savings/views.py
+++ savings/views.py
@@ -1,17 +1,27 @@
 from django.shortcuts import render
+from django.views.decorators.csrf import csrf_exempt
+
 from . import models
 
+
 # Create your views here.
+@csrf_exempt
 def test():
     """Used by Grafana to test basic connectivity"""
     return "OK"
 
+
+@csrf_exempt
 def search():
     """Used by Grafana to find metrics"""
     return '["accounts", "balances"]'
 
+
+@csrf_exempt
 def query():
     return ""
 
+
+@csrf_exempt
 def annotations():
     return "[]"
