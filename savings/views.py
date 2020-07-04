24e641d60cfdf97e47f5967a43afec1ea70da1ec
cyberjacob <cyberjacob@gmail.com>
2020-07-04 18:36:40 +0100

    Fix PK detection

--- savings/views.py
+++ savings/views.py
@@ -44,7 +44,7 @@ def query(request):
     data = json.loads(request.body)
     for target in data["targets"]:
         if target["target"] == "accounts":
-            if target["data"]["pk"] is not None:
+            if "pk" in target["data"]:
                 account = models.Account.objects.get(pk=target["data"]["pk"])
                 response.append({
                     "columns": ACCOUNT_DEF,
