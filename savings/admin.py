3faeb949e53fa3d7e2e8b6715aead795e4aec6bf
cyberjacob <cyberjacob@gmail.com>
2020-08-05 14:04:07 +0100

    Remove sorting

--- savings/admin.py
+++ savings/admin.py
@@ -31,11 +31,6 @@ class AccountAdmin(admin.ModelAdmin):
 
     actions = [update_APRs]
 
-    def get_queryset(self, request):
-        qs = super(AccountAdmin, self).get_queryset(request)
-        qs = qs.order_by(F('average_APR_localized'))
-        return qs
-
 
 class BalanceAdmin(admin.ModelAdmin):
     """Admin registration for the Balance model"""
